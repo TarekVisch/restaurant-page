1c63c4a9a6aba87cbd653dcfbbeccdfe5a6d0130
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2020-10-28 16:48:37 +0100

    Complete menu functionalities

--- src/scripts/components/navigation.js
+++ src/scripts/components/navigation.js
@@ -4,16 +4,43 @@ export const Navigation = () => {
   let isOpen = false;
 
   const toggle = () => {
+    const timeline = gsap.timeline();
+
+    const logoText = document.querySelector('.header__logo--link-text');
+    const btn = document.querySelector('.button__main');
+
     if (!isOpen) {
+      logoText.classList.add('brown');
+      btn.classList.add('brown');
+
+      // Bring navigation down
       gsap.to('.navigation', { duration: 1, y: 0, ease: 'power2.out' });
+
+      // Change from 'Menu' to 'Close'
       gsap.to('.button__main--container-inside', {
         duration: 0.5,
         y: '-50%',
         ease: 'power2.out',
       });
 
+      // Show links
+      timeline.from('.link', {
+        duration: 1,
+        delay: 1,
+        y: '100%',
+        stagger: 0.25,
+        ease: 'slow',
+      });
+
+      // Show seperators
+      timeline.from('.navigation__seperator', { duration: 0.5, opacity: 0 });
+
       isOpen = true;
     } else {
+      logoText.classList.remove('brown');
+      btn.classList.remove('brown');
+
+      // Revert back
       gsap.to('.navigation', { duration: 1, y: '-100%', ease: 'power2.out' });
       gsap.to('.button__main--container-inside', {
         duration: 0.5,

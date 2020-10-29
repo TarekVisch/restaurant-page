8c6821e79177c2ac766e5041deef52faca0d50a7
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2020-10-29 12:00:42 +0100

    Create meat section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,4 @@ import { Home } from './components/home';
 const menuButton = document.getElementById('menuButton');
 menuButton.addEventListener('click', Navigation().toggle);
 
-window.addEventListener('load', Home().handleLoad);
+// window.addEventListener('load', Home().handleLoad);
